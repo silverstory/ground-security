af567ee5148b20bd5b6ecf76ce5800ad8f0e37b8
Eprel Acer ICTO <apr.medina@gmail.com>
2020-05-26 12:48:49 +0800

    Added responsiveness to Text Widgets.

--- lib/pages/infos/basic_info.dart
+++ lib/pages/infos/basic_info.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:groundsecurity/data/model/weather.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class BasicInfo extends StatelessWidget {
   const BasicInfo({
@@ -15,7 +16,8 @@ class BasicInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double c_width = MediaQuery.of(context).size.width * 0.9;
+    double cWidth = MediaQuery.of(context).size.width * 0.9;
+    double dWidth = MediaQuery.of(context).size.width * 0.7;
     return Container(
       decoration: BoxDecoration(
         color: Colors.transparent,
@@ -39,101 +41,88 @@ class BasicInfo extends StatelessWidget {
                 ),
                 Expanded(
                   flex: 2,
-                  child: FittedBox(
-                    fit: BoxFit.contain,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly, // .end
-                      children: <Widget>[
-                        Container(
-                          width: c_width,
-                          child: Flexible(
-                            fit: FlexFit.tight,
-                            child: Text(
-                              weather.position,
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                wordSpacing: 6.0,
-                                letterSpacing: 6.0,
-                                fontSize: 21.0,
-                                // Color.fromARGB(255, 3, 54, 255),
-                                color: Color.fromRGBO(255, 255, 255, 0.6),
+                  // child: FittedBox(
+                  //   fit: BoxFit.contain,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly, // .end
+                    children: <Widget>[
+                      SizedBox(
+                        width: cWidth,
+                        height: 50.0,
+                        child: AutoSizeText(
+                          weather.position,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            wordSpacing: 6.0,
+                            letterSpacing: 6.0,
+                            fontSize: 21.0,
+                            // Color.fromARGB(255, 3, 54, 255),
+                            color: Color.fromRGBO(255, 255, 255, 0.6),
+                          ),
+                          maxLines: 2,
+                        ),
+                      ),
+                      // SizedBox(
+                      //   height: 22.0,
+                      // ),
+                      SizedBox(
+                        width: cWidth,
+                        height: 50.0,
+                        child: AutoSizeText(
+                          weather.fullName,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            wordSpacing: 6.0,
+                            letterSpacing: 3.0,
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            // Color.fromARGB(255, 30, 30, 30),
+                            color: Color.fromRGBO(255, 255, 255, 0.87),
+                          ),
+                          maxLines: 2,
+                        ),
+                      ),
+                      // SizedBox(
+                      //   height: 40.0,
+                      // ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.pin_drop,
+                            size: 35.0,
+                            // Color.fromARGB(255, 2, 53, 255),
+                            color: Color.fromARGB(255, 255, 2, 102),
+                          ),
+                          SizedBox(
+                            width: 15.0,
+                          ),
+                          SizedBox(
+                            width: dWidth, // c_width - 90,
+                            height: 50.0,
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 5.0),
+                              child: AutoSizeText(
+                                weather.office,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  wordSpacing: 6.0,
+                                  letterSpacing: 6.0,
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.bold,
+                                  // Color.fromARGB(255, 2, 53, 255),
+                                  color: Color.fromRGBO(255, 255, 255, 0.38),
+                                ),
+                                maxLines: 2,
                               ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 22.0,
-                        ),
-                        Container(
-                          width: c_width,
-                          child: Flexible(
-                            fit: FlexFit.tight,
-                            child: Text(
-                              weather.fullName,
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                wordSpacing: 6.0,
-                                letterSpacing: 3.0,
-                                fontSize: 32,
-                                fontWeight: FontWeight.bold,
-                                // Color.fromARGB(255, 30, 30, 30),
-                                color: Color.fromRGBO(255, 255, 255, 0.87),
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 40.0,
-                        ),
-                        Container(
-                          width: c_width,
-                          child: Flexible(
-                            fit: FlexFit.tight,
-                            child: Text(
-                              '📍 ' + weather.office,
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                wordSpacing: 6.0,
-                                letterSpacing: 6.0,
-                                fontSize: 28,
-                                fontWeight: FontWeight.bold,
-                                // Color.fromARGB(255, 2, 53, 255),
-                                color: Color.fromARGB(255, 114, 114, 114),
-                              ),
-                            ),
-                          ),
-                        ),
-                        // Row(
-                        //   mainAxisAlignment: MainAxisAlignment.center,
-                        //   children: <Widget>[
-                        //     Icon(
-                        //       Icons.pin_drop,
-                        //       size: 35.0,
-                        //       // Color.fromARGB(255, 2, 53, 255),
-                        //       color: Color.fromARGB(255, 255, 2, 102),
-                        //     ),
-                        //     SizedBox(
-                        //       width: 15.0,
-                        //     ),
-                        //     Text(
-                        //       weather.office,
-                        //       textAlign: TextAlign.center,
-                        //       style: TextStyle(
-                        //         wordSpacing: 6.0,
-                        //         letterSpacing: 6.0,
-                        //         fontSize: 28,
-                        //         fontWeight: FontWeight.bold,
-                        //         // Color.fromARGB(255, 2, 53, 255),
-                        //         color: Color.fromRGBO(255, 255, 255, 0.38),
-                        //       ),
-                        //     ),
-                        //   ],
-                        // ),
-                      ],
-                    ),
+                        ],
+                      ),
+                    ],
                   ),
+                  // ),
                 )
               ],
             ),
